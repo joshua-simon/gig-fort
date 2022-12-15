b81a382d398570a5ba3fe93efe937ee3e7d557f6
Joshua Simon <joshy.d.simon@gmail.com>
2022-12-16 12:03:54 +1300

    callout editing

---
+++ components/CalloutView.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const CalloutView = ({ venue,date,gigName,time }) => {
+    return ( 
+        <View style = {styles.container}>
+            <Text style = {styles.text}>{venue}</Text>
+            <Text>{gigName}</Text>
+            <Text>{`${date } ${time}`}</Text>
+        </View>
+     );
+}
+
+const styles = StyleSheet.create({
+    container: {
+     
+    }
+})
+ 
+export default CalloutView;
--- components/GigMap.js
+++ components/GigMap.js
@@ -1,21 +1,20 @@
 import { useState,useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
+import { Marker,Callout } from 'react-native-maps';
 import { query,collection,getDocs } from 'firebase/firestore';
 import { db } from '../firebase';
-import { DateTime } from 'luxon';
-import { googleMapIsInstalled } from 'react-native-maps/lib/decorateMapComponent';
+import CalloutView from './CalloutView';
 
 
 const GigMap = () => {
-  const [ gigs,setGigs ] = useState([])
+  const [gigs, setGigs] = useState([]);
 
   //Generating current date
-  const day = new Date().getDate()
-  const month = new Date().getMonth() +1;
-  const year = new Date().getFullYear()
-  const dateToday = `${day}/${month}/${year}`
+  const day = new Date().getDate();
+  const month = new Date().getMonth() + 1;
+  const year = new Date().getFullYear();
+  const dateToday = `${day}/${month}/${year}`;
 
   //Making call to Firebase to retrieve gig documents from 'gigs' collection
   useEffect(() => {
@@ -35,29 +34,40 @@ const GigMap = () => {
     getGigs();
   }, []);
 
-
-//Filtering through gigs to return only current day's gigs
-const gigsToday = gigs.filter((gig) => gig.date === dateToday)
-console.log(gigsToday)
+  //Filtering through gigs to return only current day's gigs
+  const gigsToday = gigs.filter((gig) => gig.date === dateToday);
 
   return (
-    <View style = {styles.container}>
-      <Text style = {styles.headerText}>Today's gigs</Text>
+    <View style={styles.container}>
+      <Text style={styles.headerText}>Today's gigs</Text>
       <MapView
         initialRegion={{
           latitude: -41.29416,
           longitude: 174.77782,
-          latitudeDelta: 0.08,
-          longitudeDelta: 0.08,
-        }} 
-        style = {styles.map}
+          latitudeDelta: 0.03,
+          longitudeDelta: 0.03,
+        }}
+        style={styles.map}
       >
-        {gigsToday.map((gig,i) => (
+        {gigsToday.map((gig, i) => (
           <Marker
-            key = {i}
-            coordinate = {{latitude: gig.location.latitude,longitude: gig.location.longitude}}
-            image = {require('../assets/Icon_Gold_48x48.png')}
-          />
+            key={i}
+            coordinate={{
+              latitude: gig.location.latitude,
+              longitude: gig.location.longitude,
+            }}
+            image={require("../assets/Icon_Gold_48x48.png")}
+          >
+            <Callout style={styles.callout}>
+              <CalloutView
+                venue={gig.venue}
+                date={gig.date}
+                gigName={gig.gigName}
+                time={gig.time}
+                style={styles.calloutView}
+              />
+            </Callout>
+          </Marker>
         ))}
       </MapView>
     </View>
@@ -79,6 +89,14 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize:20,
         marginTop:5
+    },
+    callout: {
+      width:200,
+      height:100,
+    },
+    calloutView: {
+      // width: 100,
+      // height:100
     }
 })
 
