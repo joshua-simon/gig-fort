4089568abd19e924da68ddf1cd1b6a85db9e1b3c
Joshua Simon <joshy.d.simon@gmail.com>
2023-03-02 19:58:18 +1300

    new icon added

--- app.config.js
+++ app.config.js
@@ -1,45 +1,41 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
-  "expo": {
-    "name": "gig-fort",
-    "slug": "gig-fort",
-    "version": "1.0.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "userInterfaceStyle": "light",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+  expo: {
+    name: "Gig Fort",
+    slug: "gig-fort",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0,
     },
-    "assetBundlePatterns": [
-      "**/*"
-    ],
-    "ios": {
-      "googleServicesFile": "./GoogleService-Info.plist",
-      "supportsTablet": true,
-      "bundleIdentifier": "android.package.gigfort"
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      googleServicesFile: "./GoogleService-Info.plist",
+      supportsTablet: true,
+      bundleIdentifier: "android.package.gigfort",
+      icon: "./assets/icon.png",
     },
-    "android": {
-      "googleServicesFile": "./google-services.json",
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
-      },
-      "package": "com.gigfort"
+    android: {
+      googleServicesFile: "./google-services.json",
+      icon: "./assets/icon.png",
+      package: "com.gigfort",
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
+    web: {
+      favicon: "./assets/favicon.png",
     },
-    "extra": {
+    extra: {
       firebaseApiKey: process.env.FIREBASE_API_KEY,
-      "eas": {
-        "projectId": "b4ab6325-8049-4f43-85bf-ac0c35b40684"
-      }
-    }
-  }
-}
+      eas: {
+        projectId: "b4ab6325-8049-4f43-85bf-ac0c35b40684",
+      },
+    },
+  },
+};
